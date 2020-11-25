a6049c743d66622884ef55db7c3f0f2a7df52915
NamH <namnh0122@gmail.com>
2020-11-25 15:22:40 +0700

    Adding yarn install and change from zsh to bash
    Signed-off-by: NamH <namnh0122@gmail.com>

--- rn_inst_macos.sh
+++ rn_inst_macos.sh
@@ -1,8 +1,8 @@
-#!/bin/zsh
+#!/bin/bash
 set -e
 set -u
 
-current_version=1.0 # Please increase me whenever you changed something
+current_version=1.1 # Please increase me whenever you changed something
 author="namh"
 
 echo "============================================================"
@@ -17,35 +17,23 @@ install_home_brew()
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 }
 
-install_node()
-{
-	echo "INSTALLING NODE"
-	brew install node
-}
-
-install_watch_man()
-{
-	echo "INSTALLING WATCHMAN"
-	brew install watchman
-}
-
 install_jre_jdk()
 {
-	echo "INSTALLING JRE, JDK 8"
-	brew tap AdoptOpenJDK/openjdk
-	brew cask install adoptopenjdk8
+  echo "INSTALLING JRE, JDK 8"
+  brew tap AdoptOpenJDK/openjdk
+  brew cask install adoptopenjdk8
 }
 
 install_react_native()
 {
-    echo "INSTALLING React Native"	
-	npm install -g react-native-cli
+  echo "INSTALLING React Native"
+  npm install -g react-native-cli
 }
 
 install_xcode_cli()
 {
-	echo "INSTALLING Xcode CLI"
-	xcode-select -install
+  echo "INSTALLING Xcode CLI"
+  xcode-select -install
 }
 
 install_visual_code()
@@ -57,29 +45,39 @@ install_visual_code()
 ### Check if already have brew or not
 which -s brew
 if [[ $? != 0 ]] ; then
-    echo "Installing HomeBrew.."
-    install_home_brew
+  echo "Installing HomeBrew.."
+  install_home_brew
+fi
+
+### Yarn
+readonly INSTALLED_BREW_PKG_FILE="installed_brew_pkgs"
+readonly YARN_KEY="yarn"
+brew list --formula > "$INSTALLED_BREW_PKG_FILE"
+
+if ! grep -q "$YARN_KEY" "$INSTALLED_BREW_PKG_FILE" ; then
+  echo "INSTALLING yarn.."
+  brew install yarn
 fi
 
 ### Check for java
 which -s java
 if [[ $? != 0 ]]; then
-  echo "Installing OpenJDK 8.."
+  echo "INSTALLING OpenJDK 8.."
   install_jre_jdk
 fi
 
 ### Check if node is installed
 which -s node
 if [[ $? != 0 ]] ; then
-	echo "Node does not installed"
-	brew install node
+  echo "INSTALLING node.."
+  brew install node
 fi
 
 ### Check if watchman is installed
 which -s watchman
 if [[ $? != 0 ]] ; then
-	echo "Watchman does not installed"
-	brew install watchman
+  echo "INSTALLING watchan.."
+  brew install watchman
 fi
 
 ### List installed node modules
@@ -90,15 +88,15 @@ readonly INSTALLED_FILE="./installed_node_pkgs"
 readonly RN_CLI="react-native-cli"
 
 if ! grep -q "$RN_CLI" "$INSTALLED_FILE" ; then
-	echo "Installing React Native cli.."
-	npm install -g react-native-cli
+  echo "Installing React Native cli.."
+  npm install -g react-native-cli
 fi
 
 ### Check if xcode cli is installed
 xcode-select -p 1>/dev/null;echo $?
 if [[ $? != 0 ]] ; then
-	echo "Installing xcode cli.."
-	xcode-select --install
+  echo "Installing xcode cli.."
+  xcode-select --install
 fi
 
 ### Check if xcode cli is installed
@@ -108,8 +106,8 @@ readonly INSTALLED_GEM_FILE="./installed_gems"
 readonly COCOAPODS="cocoapods"
 
 if ! grep -q "$COCOAPODS" "$INSTALLED_GEM_FILE" ; then
-	echo "Installing Cocoapods.."
-	sudo gem install cocoapods
+  echo "Installing Cocoapods.."
+  sudo gem install cocoapods
 fi
 
 ### export env variable to zshrc
@@ -120,14 +118,16 @@ if [[ ! -e "$ZSH_FILE" ]]; then
   touch "$ZSH_FILE"
 fi
 
+ANDROID_HOME_PATH=$HOME/Library/Android/sdk
+
 if ! grep -q "$TOKEN" "$ZSH_FILE" ; then
   echo "Exporting global env variables.."
   echo "$TOKEN" >> "$ZSH_FILE"
-  echo "export ANDROID_HOME=$HOME/Library/Android/sdk" >> "$ZSH_FILE"
-  echo "export PATH=$PATH:$ANDROID_HOME/emulator" >> "$ZSH_FILE"
-  echo "export PATH=$PATH:$ANDROID_HOsME/tools" >> "$ZSH_FILE"
-  echo "export PATH=$PATH:$ANDROID_HOME/tools/bin" >> "$ZSH_FILE"
-  echo "export PATH=$PATH:$ANDROID_HOME/platform-tools" >> "$ZSH_FILE"
+  echo "export ANDROID_HOME=$ANDROID_HOME_PATH" >> "$ZSH_FILE"
+  echo "export PATH=$PATH:$ANDROID_HOME_PATH/emulator" >> "$ZSH_FILE"
+  echo "export PATH=$PATH:$ANDROID_HOME_PATH/tools" >> "$ZSH_FILE"
+  echo "export PATH=$PATH:$ANDROID_HOME_PATH/tools/bin" >> "$ZSH_FILE"
+  echo "export PATH=$PATH:$ANDROID_HOME_PATH/platform-tools" >> "$ZSH_FILE"
 fi
 
 echo "Success! Please restart your terminal!"
